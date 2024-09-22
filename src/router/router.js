1d21037cfa9bbf9017d3e8a61acc68c7ef289822
Santiago Gallino <gallino.santi@gmail.com>
2024-09-22 00:00:59 -0300

    Clase 07: Agrega restricción de acceso a rutas según estado de autenticación

--- src/router/router.js
+++ src/router/router.js
@@ -3,17 +3,31 @@ import Home from "../pages/Home.vue";
 import Chat from "../pages/Chat.vue";
 import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
+import { subscribeToAuth } from "../services/auth";
 
 const routes = [
     { path: '/',                component: Home },
-    { path: '/chat',            component: Chat },
     { path: '/iniciar-sesion',  component: Login },
     { path: '/registro',        component: Register },
+    { path: '/chat',            component: Chat,            meta: { requiresAuth: true } },
 ]
 
 const router = createRouter({
     routes,
     history: createWebHashHistory(),
+});
+
+let loggedUser = {
+    id: null,
+    email: null,
+}
+
+subscribeToAuth(newUserData => loggedUser = newUserData)
+
+router.beforeEach((to, from) => {
+    if(to.meta.requiresAuth && loggedUser.id === null) {
+        return { path: '/iniciar-sesion' };
+    }
 });
 
 export default router;
