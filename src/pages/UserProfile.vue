e0b0be23cf601fac924741d96475dd396d62032a
Santiago Gallino <gallino.santi@gmail.com>
2024-10-06 18:13:49 -0300

    Clase 08: Agrega un loader

--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -1,10 +1,11 @@
 <script>
 import BaseHeading1 from '../components/BaseHeading1.vue';
+import BaseLoader from '../components/BaseLoader.vue';
 import { getUserProfileById } from '../services/user-profile';
 
 export default {
     name: 'UserProfile',
-    components: { BaseHeading1 },
+    components: { BaseHeading1, BaseLoader },
     data() {
         return {
             user: {
@@ -33,18 +34,23 @@ export default {
 </script>
 
 <template>
-    <BaseHeading1>Perfil de {{ user.email }}</BaseHeading1>
-
-    <div class="mb-4">
-        {{ user.bio || 'Acá va tu biografía...' }}
+    <div v-if="loading" class="p-4">
+        <BaseLoader />
     </div>
+    <template v-else>
+        <BaseHeading1>Perfil de {{ user.email }}</BaseHeading1>
 
-    <dl class="mb-4">
-        <dt class="font-bold">Email</dt>
-        <dd class="mb-2">{{ user.email }}</dd>
-        <dt class="font-bold">Nombre</dt>
-        <dd class="mb-2">{{ user.displayName || 'No especificado' }}</dd>
-        <dt class="font-bold">Carrera</dt>
-        <dd class="mb-2">{{ user.career || 'No especificado' }}</dd>
-    </dl>
+        <div class="mb-4">
+            {{ user.bio || 'Acá va tu biografía...' }}
+        </div>
+
+        <dl class="mb-4">
+            <dt class="font-bold">Email</dt>
+            <dd class="mb-2">{{ user.email }}</dd>
+            <dt class="font-bold">Nombre</dt>
+            <dd class="mb-2">{{ user.displayName || 'No especificado' }}</dd>
+            <dt class="font-bold">Carrera</dt>
+            <dd class="mb-2">{{ user.career || 'No especificado' }}</dd>
+        </dl>
+    </template>
 </template>
