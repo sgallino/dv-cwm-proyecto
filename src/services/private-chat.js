095d9ce4b630367816a57499b84fccc12035d7c7
Santiago Gallino <gallino.santi@gmail.com>
2024-11-03 21:13:03 -0300

    Clase 11: Implementa caché para los documentos de los chats

--- src/services/private-chat.js
+++ src/services/private-chat.js
@@ -1,8 +1,29 @@
 import { addDoc, collection, getDocs, limit, onSnapshot, orderBy, query, serverTimestamp, where } from "firebase/firestore";
 import { db } from "./firebase";
 
-// TODO: Cachear el resultado para optimizar performance.
+let documentCache = {};
+
+function getCacheKey(senderId, receiverId) {
+    return [senderId, receiverId].sort().join("_");
+}
+
+function putInCache(key, value) {
+    documentCache[key] = value;
+}
+
+function retrieveFromCache(key) {
+    return documentCache[key] || null;
+}
+
 async function getChatDocument(senderId, receiverId) {
+    const cacheKey = getCacheKey(senderId, receiverId);
+    const cachedDoc = retrieveFromCache(cacheKey);
+
+    if(cachedDoc) {
+        console.log("Obteniendo la conversación del caché.");
+        return cachedDoc
+    }
+
     const users = {
         [senderId]: true,
         [receiverId]: true,
@@ -13,14 +34,21 @@ async function getChatDocument(senderId, receiverId) {
     const chatQuery = query(chatsRef, where('users', '==', users), limit(1));
 
     const chatSnapshot = await getDocs(chatQuery);
+    
+    let chatDoc;
 
     if(chatSnapshot.empty) {
-        return await addDoc(chatsRef, {
+        chatDoc = await addDoc(chatsRef, {
             users: users
         });
     }
 
-    return chatSnapshot.docs[0];
+    chatDoc = chatSnapshot.docs[0];
+
+    console.log("Conversación obtenida de la base de datos.");
+    putInCache(cacheKey, chatDoc);
+
+    return chatDoc;
 }
 
 export async function privateChatSaveMessage(senderId, receiverId, content) {
