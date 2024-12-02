ea2ce6b8d64490051b0f5629b939ce7b4180e4f0
Santiago Gallino <gallino.santi@gmail.com>
2024-12-02 01:07:01 -0300

    Clase 13: Agrega variantes de BaseButton

--- src/components/form/BaseButton.vue
+++ src/components/form/BaseButton.vue
@@ -1,7 +1,27 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    variant: {
+        type: String,
+        default: 'primary',
+    }
+});
+
+const VARIANT_COLORS = {
+    primary: 'bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 active:bg-blue-700',
+    success: 'bg-green-500 hover:bg-green-400 focus:bg-green-400 active:bg-green-700',
+    error: 'bg-red-500 hover:bg-red-400 focus:bg-red-400 active:bg-red-700',
+}
+
+const variantColors = computed(() => VARIANT_COLORS[props.variant]);
+</script>
+
 <template>
     <button
         type="submit"
-        class="py-2 px-4 rounded bg-blue-500 text-white hover:bg-blue-400 focus:bg-blue-400 active:bg-blue-700"
+        class="py-2 px-4 rounded text-white"
+        :class="variantColors"
     >
         <slot>Enviar</slot>
     </button>
