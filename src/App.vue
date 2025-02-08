5e30ba808a91a32f49d44aa58bdecbad91ec7069
Santiago Gallino <gallino.santi@gmail.com>
2025-02-08 17:48:02 -0300

    Clase 15: Agrega manejo de notificaciones globales de feedback en App.vue con el componente NotificationBox

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
 import AppNavbar from './components/AppNavbar.vue';
 import AppFooter from './components/AppFooter.vue';
+import { ref } from 'firebase/storage';
+import NotificationBox from './components/NotificationBox.vue';
+
+const feedback = ref({
+    message: null,
+    type: null,
+});
 </script>
 
 <template>
     <AppNavbar />
     <main class="container mx-auto p-4">
+        <NotificationBox 
+            v-if="feedback.message"
+            :content="feedback" 
+        />
+
         <router-view />
     </main>
     <AppFooter />
