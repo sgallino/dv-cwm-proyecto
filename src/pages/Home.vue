c71d7473e3275ea787f5826b4d6349cefdb7fbfa
Santiago Gallino <gallino.santi@gmail.com>
2025-02-09 00:45:47 -0300

    Clase 16: Agrega un texto de bienvenida a la home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,4 +4,7 @@ import BaseHeading1 from '../components/BaseHeading1.vue';
 
 <template>
     <BaseHeading1>Página Principal de DV Social</BaseHeading1>
+
+    <p class="mb-4">¡Les damos la bienvenida a la mejor red social académica!</p>
+    <p>Acá van a poder encontrar todas las últimas noticias, anuncios, etc.</p>
 </template>
